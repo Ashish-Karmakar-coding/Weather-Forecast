50d7a61806e1637472c5f00392881e5ce6da92b1
~Kenshin <ashish.karmakar782@gmail.com>
2025-02-05 22:24:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const BASE_URL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
+const apiKey = "e61f74672405576b267e3cfaa70aa062";
+
+
+let city = document.querySelector(".upper input").value;
+let btn = document.querySelector(".upper button");
+let img = document.querySelector(".img");
+let temp = document.querySelector(".temp .temp_value");
+let cityName = document.querySelector(".city");
+let humidity = document.querySelector(".HUV");
+let windSpeed = document.querySelector(".WSV");
+
+const weatherCheck = async() => {
+    const response = await fetch(BASE_URL + city + `&appid=${apiKey}`);
+    let data = response.json();
+    console.log(data);
+
+    cityName.innerText = data.name;
+    temp.innerText = Math.round(data.main.temp) + "°c";
+    humidity.innerText = data.main.humidity + "%";
+    windSpeed.innerText = data.wind.speed + "km/h";
+
+    if(data.weather[0].main === "Clouds"){
+        img.src = "clouds.png";
+    }else if(data.weather[0].main === "Clear"){
+        img.src = "clear.png";
+    }else if(data.weather[0].main === "Rain"){
+        img.src = "rain.png";
+    }else if(data.weather[0].main === "Drizzle"){
+        img.src = "drizzel.png";
+    }else if(data.weather[0].main === "Mist"){
+        img.src = "mist.png";
+    }
+
+    // document.querySelector(".weather").style.display = "none";
+    
+}
+
+btn.addEventListener("click" , () =>{
+    weatherCheck();
+});
